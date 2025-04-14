5645164b98e97933e107a1fc6c0ac2e6f624d0e8
Cyenoch <jgbingzi@outlook.com>
2025-04-14 18:58:56 +0800

    remove auto import

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from '@tanstack/react-start/config'
-import autoImport from 'unplugin-auto-import/vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
@@ -26,13 +25,6 @@ export default defineConfig({
     plugins: [
       tsConfigPaths({
         projects: ['./tsconfig.json'],
-      }),
-
-      autoImport({
-        imports: [
-          'react',
-        ],
-        dts: 'app/types/auto-imports.d.ts',
       }),
     ],
   },
