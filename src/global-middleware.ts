9145864047a5e3eb49be0033c2d5aaad1d11f667
Cyenoch <jgbingzi@outlook.com>
2025-04-27 13:40:56 +0800

    Refactors global middleware registration to use spread operator for improved clarity

--- src/global-middleware.ts
+++ src/global-middleware.ts
@@ -1,7 +1,6 @@
-import type { AnyMiddleware } from '@tanstack/react-start'
 import { registerGlobalMiddleware } from '@tanstack/react-start'
 import { appMiddlewares } from './server/middleware/global'
 
 registerGlobalMiddleware({
-  middleware: appMiddlewares as unknown as AnyMiddleware[],
+  middleware: [...appMiddlewares],
 })
