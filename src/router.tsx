017a1f33314ff277d308cb9f52c127b6114bac40
Cyenoch <jgbingzi@outlook.com>
2025-04-29 10:43:14 +0800

    Refactors query client initialization to use a factory function for improved flexibility

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,11 @@ import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routerWithQueryClient } from '@tanstack/react-router-with-query'
 import ErrorPrint from './components/core/error-print'
 import NotFound from './components/core/not-found'
-import { queryClient } from './lib/utils/query-client'
+import { createQueryClient } from './lib/utils/query-client'
 import { routeTree } from './routeTree.gen'
 
 export function createRouter() {
+  const queryClient = createQueryClient()
   const router = routerWithQueryClient(
     createTanStackRouter({
       routeTree,
