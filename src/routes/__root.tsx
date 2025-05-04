83af1a08bb7fe841aff10fd9f67ab54aec4400bf
Cyenoch <jgbingzi@outlook.com>
2025-05-04 17:51:47 +0800

    fix: update viewport meta tag to prevent user scaling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -19,7 +19,7 @@ export const Route = createRootRouteWithContext<{
       },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        content: 'width=device-width, initial-scale=1, user-scalable=no',
       },
       ...seo({
         title: '[ReactTemplate]',
