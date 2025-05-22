5590a11b4da990b613cc7291030e25f38689f04e
Ryu <jgbingzi@outlook.com>
2025-05-22 11:20:10 +0800

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,14 @@
+import { createIsomorphicFn } from '@tanstack/react-start'
 import z from 'zod'
 
-export const Env = z.object({
-  LOG_LEVEL: z.string().min(3).max(10).default('trace'),
-  DATABASE_URL: z.string().url(),
-  AUTH_SECRET: z.string().min(32).max(256),
-}).parse(Bun.env)
+export const Env = createIsomorphicFn().server(() => {
+  return z.object({
+    LOG_LEVEL: z.string().min(3).max(10).default('trace'),
+    DATABASE_URL: z.string().url(),
+    AUTH_SECRET: z.string().min(32).max(256),
+  }).parse(Bun.env)
+}).client(() => {
+  z.object({
+
+  }).parse(import.meta.env)
+})()
